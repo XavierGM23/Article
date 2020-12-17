5ea9e20314530e7bc43520fb9e7f28d183da5689
XavierGM23 <64241384+XavierGM23@users.noreply.github.com>
2020-12-17 17:46:52 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.querySelector(".icon").addEventListener("click", ()=> {
+  document.querySelector(".icon").classList.toggle("icon-hover");
+  document.querySelector(".hide").classList.toggle("show");
+});
